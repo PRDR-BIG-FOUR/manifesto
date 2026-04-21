4a6d73c91060050aff126be576d181ab93bb92fc
Rakesh <59800595+rakeshpraneel@users.noreply.github.com>
2026-04-21 12:33:37 +0530

    Refactor PostCSS config for Tailwind CSS support
    Updated PostCSS configuration to use Tailwind CSS and Autoprefixer.

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -14,11 +14,18 @@
  */
 // export default {}
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/manifesto/', // This MUST match your repo name
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/manifesto/', // This MUST match your repo name
+// })
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
